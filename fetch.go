3be2cdddc7f1f918d1972ccded9df361cc871e43
Kirill <kir.karasyov2017@yandex.ru>
2024-08-29 20:48:10 +0300

    Made it perfect

--- fetch.go
+++ fetch.go
@@ -149,6 +149,25 @@ func PrintLine(w *bufio.Writer, key, val string) {
 func main() {
 	w := bufio.NewWriter(os.Stdout)
 	defer w.Flush()
+
+	file, err := os.Open("logo.txt")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Println(line)
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading file:", err)
+	}
+
 	PrintLine(w, "OS:      ", GetOS())
 	PrintLine(w, "Kernel:  ", GetKernel())
 	PrintLine(w, "CPU:     ", GetCPU())
