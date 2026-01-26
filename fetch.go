925fa77b623d6528a7cab1180d91630db6a3fc8a
Kirill Karasev <kirill@MacBook-Air-Kirill.local>
2026-01-27 02:53:34 +0300

    Added MacOS support

--- fetch.go
+++ fetch.go
@@ -9,8 +9,10 @@ import (
 	"os/exec"
 	"path/filepath"
 	"regexp"
+	"runtime"
 	"strconv"
 	"strings"
+	"time"
 )
 
 const (
@@ -19,12 +21,12 @@ const (
 )
 
 func GetPatternsFromFile(path string, patterns ...string) ([]string, error) {
-	f, err := os.Open(path)
+	f, err := os.ReadFile(path)
 	if err != nil {
 		log.Printf("opening file - '%s': %v\n", path, err)
 		return nil, errors.New("couldn't get patterns")
 	}
-	defer f.Close()
+	f_str := string(f)
 
 	res := make([]*regexp.Regexp, 0, len(patterns))
 	for _, pattern := range patterns {
@@ -32,19 +34,11 @@ func GetPatternsFromFile(path string, patterns ...string) ([]string, error) {
 	}
 
 	matches := make([]string, len(patterns))
-	bs := bufio.NewScanner(f)
-	for bs.Scan() {
-		for i, re := range res {
-			match := re.FindStringSubmatch(bs.Text())
-			if len(match) > 0 {
-				matches[i] = match[1]
-			}
+	for i, re := range res {
+		match := re.FindStringSubmatch(f_str)
+		if len(match) > 0 {
+			matches[i] = match[1]
 		}
-	}
-
-	if err := bs.Err(); err != nil {
-		log.Printf("reading file - '%s': %v\n", path, err)
-		return nil, errors.New("couldn't get patterns")
 	}
 	return matches, nil
 }
@@ -67,12 +61,25 @@ func ParseInt(s string) int {
 	return n
 }
 
-func GetOS() string {
-	osMatches, err := GetPatternsFromFile(
-		"/etc/os-release",
-		"^NAME=\"?([^\"]+)", "^PRETTY_NAME=\"?([^\"]+)",
-		"^VERSION=\"?([^\"]+)", "^VERSION_ID=\"?([^\"]+)",
-	)
+func GetOS(path, os string) string {
+	var osMatches []string
+	var err error
+	switch os {
+	case "darwin":
+		osMatches, err = GetPatternsFromFile(
+			path,
+			`(?s)<key>ProductName</key>\s*<string>(.*?)</string>`,
+			`(?s)<key>ProductName</key>\s*<string>(.*?)</string>`,
+			`(?s)<key>ProductUserVisibleVersion</key>\s*<string>(.*?)</string>`,
+			`(?s)<key>ProductVersion</key>\s*<string>(.*?)</string>`,
+		)
+	default:
+		osMatches, err = GetPatternsFromFile(
+			path,
+			"^NAME=\"?([^\"]+)", "^PRETTY_NAME=\"?([^\"]+)",
+			"^VERSION=\"?([^\"]+)", "^VERSION_ID=\"?([^\"]+)",
+		)
+	}
 	var sOs, sVer string
 	if err != nil {
 		sOs = "Uknown OS"
@@ -106,36 +113,105 @@ func GetKernel() string {
 	return s_kernel
 }
 
-func GetCPU() string {
-	matches, err := GetPatternsFromFile("/proc/cpuinfo", "model name\\s+: (.+)")
-	if err != nil {
-		return "Uknown CPU"
+func GetCPU(os string) string {
+	switch os {
+	case "darwin":
+		matches, err := RunCMD("sysctl", "-n", "machdep.cpu.brand_string")
+		if err != nil {
+			return "Uknown CPU"
+		}
+		return matches
+	default:
+		matches, err := GetPatternsFromFile("/proc/cpuinfo", "model name\\s+: (.+)")
+		if err != nil {
+			return "Uknown CPU"
+		}
+		return matches[0]
 	}
-	return matches[0]
 }
 
-func GetMem() string {
-	s_mem, err := GetPatternsFromFile(
-		"/proc/meminfo", "MemTotal:\\s+([0-9]+)", "MemAvailable:\\s+([0-9]+)",
-	)
-	if err != nil {
-		return "Memory Info is Unavailable"
-	}
-	mem_total := float64(ParseInt(s_mem[0]))
-	mem_used := mem_total - float64(ParseInt(s_mem[1]))
-	mem_total /= 1024.0 * 1024.0
-	mem_used /= 1024.0 * 1024.0
-	mem_percent := mem_used * 100.0 / mem_total
+func GetMem(sOs string) string {
+	switch sOs {
+	case "darwin":
+		s_mem_total, err := RunCMD("sysctl", "-n", "hw.memsize")
+		if err != nil {
+			return "Memory Info is Unavailable"
+		}
+		mem_total := float64(ParseInt(s_mem_total))
 
-	return fmt.Sprintf("%.2f / %.2f Gi (%.0f%%)", mem_used, mem_total, mem_percent)
+		page_size := os.Getpagesize()
+		vmOut, err_vm := RunCMD("vm_stat")
+		if err_vm != nil {
+			return "Memory Info is Unavailable"
+		}
+
+		reWired := regexp.MustCompile(`Pages wired down:\s+(\d+)`)
+		reActive := regexp.MustCompile(`Pages active:\s+(\d+)`)
+		reOccupied := regexp.MustCompile(`Pages occupied by compressor:\s+(\d+)`)
+
+		wiredMatch := reWired.FindStringSubmatch(vmOut)
+		activeMatch := reActive.FindStringSubmatch(vmOut)
+		occupiedMatch := reOccupied.FindStringSubmatch(vmOut)
+
+		var totalUsedPages int
+
+		if len(wiredMatch) > 1 {
+			v := ParseInt(wiredMatch[1])
+			totalUsedPages += v
+		}
+		if len(activeMatch) > 1 {
+			v := ParseInt(activeMatch[1])
+			totalUsedPages += v
+		}
+		if len(occupiedMatch) > 1 {
+			v := ParseInt(occupiedMatch[1])
+			totalUsedPages += v
+		}
+		mem_used := float64(totalUsedPages * page_size)
+
+		mem_total /= 1024.0 * 1024.0 * 1024.0
+		mem_used /= 1024.0 * 1024.0 * 1024.0
+		mem_percent := mem_used * 100.0 / mem_total
+		return fmt.Sprintf("%.2f / %.2f Gi (%.0f%%)", mem_used, mem_total, mem_percent)
+
+	default:
+		s_mem, err := GetPatternsFromFile(
+			"/proc/meminfo", "MemTotal:\\s+([0-9]+)", "MemAvailable:\\s+([0-9]+)",
+		)
+		if err != nil {
+			return "Memory Info is Unavailable"
+		}
+		mem_total := float64(ParseInt(s_mem[0]))
+		mem_used := mem_total - float64(ParseInt(s_mem[1]))
+		mem_total /= 1024.0 * 1024.0
+		mem_used /= 1024.0 * 1024.0
+		mem_percent := mem_used * 100.0 / mem_total
+		return fmt.Sprintf("%.2f / %.2f Gi (%.0f%%)", mem_used, mem_total, mem_percent)
+	}
 }
 
-func GetUptime() string {
-	s_seconds, err := GetPatternsFromFile("/proc/uptime", "^([0-9]+)")
-	if err != nil {
-		return "Runtime Info is Unavailable"
+func GetUptime(sOs string) string {
+	var seconds int
+	switch sOs {
+	case "darwin":
+		out, err := RunCMD("sysctl", "-n", "kern.boottime")
+		if err != nil {
+			return "Runtime Info is Unavailable"
+		}
+		re := regexp.MustCompile(`sec = (\d+)`)
+		match := re.FindStringSubmatch(string(out))
+		if len(match) > 1 {
+			bootTimestamp := ParseInt(match[1])
+
+			seconds = int(time.Now().Unix()) - bootTimestamp
+		}
+	default:
+		s_seconds, err := GetPatternsFromFile("/proc/uptime", "^([0-9]+)")
+		if err != nil {
+			return "Runtime Info is Unavailable"
+		}
+		seconds = ParseInt(s_seconds[0])
 	}
-	seconds := ParseInt(s_seconds[0])
 	mins := seconds / 60 % 60
 	hours := seconds / 60 / 60 % 24
 	days := seconds / 60 / 60 / 24
@@ -184,12 +260,24 @@ func PrintLine(w *bufio.Writer, key, val string) {
 
 func main() {
 	w := bufio.NewWriter(os.Stdout)
+	sOs := runtime.GOOS
+	var os_path string
 	defer w.Flush()
-	PrintLine(w, "OS:      ", GetOS())
+	switch sOs {
+	case "linux":
+		os_path = "/etc/os-release"
+	case "darwin":
+		os_path = "/System/Library/CoreServices/SystemVersion.plist"
+	case "windows":
+		os_path = "Registry"
+	default:
+		os_path = sOs
+	}
+	PrintLine(w, "OS:      ", GetOS(os_path, sOs))
 	PrintLine(w, "Kernel:  ", GetKernel())
-	PrintLine(w, "CPU:     ", GetCPU())
-	PrintLine(w, "Memory:  ", GetMem())
-	PrintLine(w, "Uptime:  ", GetUptime())
+	PrintLine(w, "CPU:     ", GetCPU(sOs))
+	PrintLine(w, "Memory:  ", GetMem(sOs))
+	PrintLine(w, "Uptime:  ", GetUptime(sOs))
 	PrintLine(w, "Shell:   ", GetShell())
 	PrintLine(w, "Packages:", GetPortage())
 }
